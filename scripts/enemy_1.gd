1c24f9049789ad35f1e25928a49b1e267f8f3c01
Walter <robinwehrle@gmail.com>
2025-06-15 18:28:45 +0200

    enemy1 shoots now
    in enemy1.gd: abgelaufener shot_timer löst shot() aus:
    -initiiert enemy_shot_basic.tcsn
    -passt Grösse an
    -spielt Schuss Sound ab (AudioStream -> Exortvariable)

--- scripts/enemy_1.gd
+++ scripts/enemy_1.gd
@@ -1,12 +1,21 @@
 extends Area2D
 
 @onready var explosion_animation = preload("res://scenes/explosion_animation.tscn").instantiate()
+@onready var explosion_size : float = 5
+@onready var speed = basic_speed * GameManager.loop_counter
+@onready var audio_stream_player_2d = $AudioStreamPlayer2D
+
+
+
+@export var shot_sound : AudioStream 
+@export var shot_scene : PackedScene
 @export var damage = 100
 @export var basic_speed : int = 50
 @export var score_count : int = 100
 @export var energy_left : int = 5
-@onready var explosion_size : float = 5
-@onready var speed = basic_speed * GameManager.loop_counter
+
+var shoot_timer = Timer.new()
+
 
 signal enemy_destroyed(score: int, energy: int)
 
@@ -15,6 +24,11 @@ func _ready() -> void:
 	area_entered.connect(_on_area_entered)
 	add_to_group("one_hit_enemies")
 	add_to_group("enemies")
+	add_child(shoot_timer)
+	shoot_timer.wait_time = 2
+	shoot_timer.timeout.connect(_on_shoot_timer_timeout)
+	shoot_timer.start()
+	audio_stream_player_2d.stream = shot_sound
 	
 
 func _on_area_entered(area: Area2D) -> void:
@@ -29,3 +43,9 @@ func _on_area_entered(area: Area2D) -> void:
 func _process(delta: float) -> void:
 	position.x -= delta * speed
 	
+
+func _on_shoot_timer_timeout():
+	audio_stream_player_2d.play(2)
+	var shot = shot_scene.instantiate()
+	shot.scale = Vector2(3, 3)
+	add_child(shot)
