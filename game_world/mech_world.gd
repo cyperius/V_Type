46b2a739fef45d4949e64ead18228c6f37e3f0db
Walter <robinwehrle@gmail.com>
2026-01-04 18:03:29 +0100

    Add random rotation direction to girders
    Girders now receive a random rotation direction upon respawn, making their movement less predictable. Also adjusted the respawn Y position range and tweaked speed/rotation factor increments. Updated initial position of an AnimatableSteelGirder in level 7.

--- game_world/mech_world.gd
+++ game_world/mech_world.gd
@@ -20,7 +20,7 @@ var chosen_max_move_speed_factor : float
 var girders_stats : Dictionary = {}
 var move_direction_y : float = 0 # wird in set_girder_stats() zufällig gesetzt
 var move_direction_y_range : float = 0 # wird in set_grider_stats schrittweise erhöht
-
+var rotation_direction : int = 1
 
 func _ready() -> void:
 	girders = steel_girders.get_children()
@@ -33,6 +33,7 @@ func _ready() -> void:
 		girders_stats[girder]["move_speed"] = basic_move_speed * randi_range(min_move_speed_factor, max_rotation_speed)
 		girders_stats[girder]["rotation_speed"] = randf_range(min_rotation_speed, max_rotation_speed)
 		girders_stats[girder]["move_direction_y"] = 0
+		girders_stats[girder]["rotation_direction"] = rotation_direction
 		set_girder_stats_range()
 		var girder_area = girder.get_node("Area2D")
 		girder_area.area_entered.connect(_on_area_entered)
@@ -44,25 +45,27 @@ func _physics_process(delta: float) -> void:
 	for girder in girders: # Bewegung der Girders; Werterange wird jeweils in set_girders_stats_range
 		# erweitert und danach werden die Werte zufällig neu gesetzt
 		girder.position += girders_stats[girder]["move_speed"] * delta * Vector2(-1, girders_stats[girder]["move_direction_y"])
-		girder.rotation += girders_stats[girder]["rotation_speed"] * delta
+		girder.rotation += girders_stats[girder]["rotation_speed"] * delta * girders_stats[girder]["rotation_direction"]
 		if girder.position.x <= -200 or girder.position.y < -200 or girder.position.y > 2600: 
 			# wenn ausserhalb des Bildes: neue Zufallsstats setzen und rechts respawnen
 			set_girder_stats_range() # respawnen, Werte-range erweitern, danach werden Werte zufällig gesetzt
 			girders_stats[girder]["move_speed"] = randi_range(min_move_speed_factor, chosen_max_move_speed_factor) * basic_move_speed
 			girders_stats[girder]["rotation_speed"] = randf_range(min_rotation_speed, max_rotation_speed)
 			girders_stats[girder]["move_direction_y"] = randf_range(-1 * move_direction_y_range, move_direction_y_range)
+			girders_stats[girder]["rotation_direction"] = [1, -1].pick_random()
 			girder.position.x = 4300
-			girder.position.y = randi_range(600, 1600)
+			girder.position.y = randi_range(100, 2400)
 			print("grider_move_speed ", girders_stats[girder]["move_speed"])
 			
 			
 func set_girder_stats_range() -> void:
-	min_move_speed_factor += 0.05 # range für Geschwindigkeiten erhöhen 
+	#min_move_speed_factor += 0.01 # range für Geschwindigkeiten erhöhen 
 	# (da Endgeschwindigkeit über rangi_rage berechnet wird, erst ab 10 Durchläufen wirksam)
-	possible_max_move_speed_factor += 0.05
+	possible_max_move_speed_factor += 0.01
 	chosen_max_move_speed_factor = randf_range(min_move_speed_factor, possible_max_move_speed_factor)
-	min_rotation_speed += 0.01 # range für Rotationsgeschwindîgkeit erhöhen
+	#min_rotation_speed += 0.01 # range für Rotationsgeschwindîgkeit erhöhen
 	max_rotation_speed += 0.01
+	max_rotation_speed = clamp(max_rotation_speed, max_rotation_speed, 6)
 	# y-Bewegung: mögliche range erhöhen und Richtung zufällig wählen lassen
 	move_direction_y_range += 0.01
 	move_direction_y_range = clamp(move_direction_y_range, move_direction_y_range, 0.2)
