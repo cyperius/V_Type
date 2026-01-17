b01576fb908a2ea1c3b90bc4ac9a664482697054
Walter <robinwehrle@gmail.com>
2026-01-17 13:14:42 +0100

    Add burst animation and golden girder assets
    Added new burst animation frames for the player, golden girder textures, and a new tilemap texture. Updated steel girder scene to use the golden girder asset, adjusted related level and collision settings, and removed unused animatable steel girder scripts. Integrated new tilemap into level 8.

--- scripts/steel_girder_area_2d.gd
+++ scripts/steel_girder_area_2d.gd
@@ -2,5 +2,6 @@ extends Area2D
 
 @export var damage : int = 1000
 
-#func _ready() -> void:
-	#add_to_group("obstacles")
+func _ready() -> void:
+	var skin = get_child(0) as Sprite2D
+	skin.texture = load("res://assets/graphic_elements/game_world/golden_girder_256x171.png")
